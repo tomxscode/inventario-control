82eeb568b4ded6e75ba9491402c62e3a152a1423
Tomás Barros <tomasgalleguillos1309@gmail.com>
2023-07-24 17:13:23 -0400

    Agregada tabla categorías

--- bd.sql
+++ bd.sql
@@ -1,9 +1,15 @@
 CREATE DATABASE inventario_control;
 USE inventario_control;
 
+CREATE TABLE categorias (
+    id int primary key identity(1),
+    concepto varchar(128) unique not null,
+    descripcion varchar(256) null
+);
+
 CREATE TABLE productos (
     sku varchar(32) primary key,
     detalle varchar(128) not null,
     descripcion varchar(256) null,
     existencias int default(0) not null
-)
+);
